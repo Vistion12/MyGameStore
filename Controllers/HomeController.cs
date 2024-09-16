439e8441f21b9df4743460aacbac554649ed8fda
Nikita Star <MAILn89190245729@gmail.com>
2024-09-16 17:05:13 +0300

    заделали фильтрацию по жанрам и допилили поиск, и дополнил базу играми

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyGameStore.Extensions;
+using MyGameStore.ViewModel;
 using MyGameStoreModel.Data;
 using MyGameStoreModel.Entities;
 using MyGameStoreModel.Repositories.Interfaces;
@@ -15,13 +17,41 @@ public class HomeController(GameShopContext gameShopContext,
     IGameProductRepository gameProductRepository,
     IHttpContextAccessor httpContextAccessor) : Controller
 {
-
+    private const int minValueMonth = -1;
     private const int countPopGames = 100;
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string GameGenre, string NameSearchString)
     {
-        var gameProducts = await gameProductRepository.GetAllGameProductsAsync();
-        return View(gameProducts);
+		var gameProductGenre = from g in gameShopContext.Genres
+						   select g;
+
+		var gameProducts = from g in gameShopContext.GameProducts
+                           select g;
+
+        if (!string.IsNullOrEmpty(NameSearchString))
+        {
+            gameProducts = gameProducts.Where(
+                gameProduct => 
+                gameProduct.Title.ToUpper().Contains(NameSearchString.ToUpper()));
+        }
+
+		if (!string.IsNullOrEmpty(GameGenre))
+		{
+			gameProducts = gameProducts
+                .Include(gameProduct => gameProduct.Genre)
+                .Where(gameProduct => 
+                            gameProduct.Genre.Where(genre=> 
+                                    genre.Title.Contains(GameGenre)).Any());
+		}
+
+		var filteredGameProductVM = new FilteredGameProductVM
+        { 
+            GameGenres = new SelectList(await gameProductGenre.Select(genre=>genre.Title).ToListAsync()) ,
+            GameProducts= await gameProducts.ToListAsync() 
+        };
+
+        
+        return View(filteredGameProductVM);
     }
 
     //public async Task<IActionResult> Index(string searchString)
@@ -33,30 +63,18 @@ public class HomeController(GameShopContext gameShopContext,
 
     public async Task<IActionResult> PopularGames()
     {
-        //var currentDate = DateTime.UtcNow;
-        //var monthAgo = new DateTime(currentDate.Year, currentDate.Month - 1, currentDate.Day);
 
         var Carts = await gameShopContext.carts
              .Include(cart => cart.gameProducts)
-             .Between(cart => cart.DatePurchase, DateTime.UtcNow.Date, DateTime.UtcNow.Date)
+             .Where(cart => 
+             cart.DatePurchase >= DateTime.UtcNow.Date.AddMonths(minValueMonth)&&
+             cart.DatePurchase <= DateTime.UtcNow.Date)
              .ToListAsync();
-        var dictionary = new Dictionary<int, GameProduct>();
-        foreach (var cart in Carts)
-        {
-            if (dictionary.Count > 100)
-            {
-                break;
-            }
-            foreach (var gameProduct in cart.gameProducts)
-            {
-                if (dictionary.ContainsKey(gameProduct.Id))
-                {
-                    continue;
-                }
-                dictionary.Add(gameProduct.Id,gameProduct);
-            }
 
-        }
+        var dictionary = Carts.SelectMany(cart => cart.gameProducts)
+            .GroupBy(gameProduct => gameProduct.Id)
+            .Take(countPopGames)
+            .ToDictionary(group => group.Key, group => group.First());
 
         return View(dictionary);
     }
---
+++ ViewModel/FilteredGameProductVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyGameStoreModel.Entities;
+
+namespace MyGameStore.ViewModel;
+
+public class FilteredGameProductVM
+{
+	public string? NameSearchString { get; set; }
+	public string? GameGenre { get; set; }
+	public required List <GameProduct> GameProducts { get; set; }
+	public required SelectList GameGenres { get; set; }	
+}
